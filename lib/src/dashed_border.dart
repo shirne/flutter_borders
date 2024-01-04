b2b168744d05fc1d1345fe90425c8c2495757606
shirne <shirne@126.com>
2024-01-04 10:37:45 +0800

    add dashed border(dev)

---
+++ lib/src/dashed_border.dart
@@ -0,0 +1,573 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+import 'ui/styled_border_side.dart';
+
+class DashedBorder extends Border {
+  /// Creates a border.
+  ///
+  /// All the sides of the border default to [StyledBorderSide.none].
+  ///
+  /// The arguments must not be null.
+  const DashedBorder({
+    this.top = StyledBorderSide.none,
+    this.right = StyledBorderSide.none,
+    this.bottom = StyledBorderSide.none,
+    this.left = StyledBorderSide.none,
+  });
+
+  /// Creates a border whose sides are all the same.
+  ///
+  /// The `side` argument must not be null.
+  const DashedBorder.fromBorderSide(StyledBorderSide side)
+      : this(
+          top: side,
+          right: side,
+          bottom: side,
+          left: side,
+        );
+
+  /// Creates a border with symmetrical vertical and horizontal sides.
+  ///
+  /// The `vertical` argument applies to the [left] and [right] sides, and the
+  /// `horizontal` argument applies to the [top] and [bottom] sides.
+  ///
+  /// All arguments default to [BorderSide.none] and must not be null.
+  const DashedBorder.symmetric({
+    StyledBorderSide vertical = StyledBorderSide.none,
+    StyledBorderSide horizontal = StyledBorderSide.none,
+  }) : this(
+          left: vertical,
+          top: horizontal,
+          right: vertical,
+          bottom: horizontal,
+        );
+
+  /// A uniform border with all sides the same color and width.
+  ///
+  /// The sides default to black solid borders, one logical pixel wide.
+  factory DashedBorder.all({
+    Color color = const Color(0xFF000000),
+    double width = 1.0,
+    BorderStyle style = BorderStyle.solid,
+    double strokeAlign = BorderSide.strokeAlignInside,
+    BorderDash? dashStyle,
+  }) {
+    final side = StyledBorderSide(
+      color: color,
+      width: width,
+      style: style,
+      strokeAlign: strokeAlign,
+      dashStyle: dashStyle,
+    );
+    return DashedBorder.fromBorderSide(side);
+  }
+
+  @override
+  final StyledBorderSide left;
+  @override
+  final StyledBorderSide top;
+  @override
+  final StyledBorderSide right;
+  @override
+  final StyledBorderSide bottom;
+
+  /// Creates a [DashedBorder] that represents the addition of the two given
+  /// [DashedBorder]s.
+  ///
+  /// It is only valid to call this if [BorderSide.canMerge] returns true for
+  /// the pairwise combination of each side on both [DashedBorder]s.
+  ///
+  /// The arguments must not be null.
+  static DashedBorder merge(DashedBorder a, DashedBorder b) {
+    assert(BorderSide.canMerge(a.top, b.top));
+    assert(BorderSide.canMerge(a.right, b.right));
+    assert(BorderSide.canMerge(a.bottom, b.bottom));
+    assert(BorderSide.canMerge(a.left, b.left));
+    return DashedBorder(
+      top: StyledBorderSide.merge(a.top, b.top),
+      right: StyledBorderSide.merge(a.right, b.right),
+      bottom: StyledBorderSide.merge(a.bottom, b.bottom),
+      left: StyledBorderSide.merge(a.left, b.left),
+    );
+  }
+
+  @override
+  EdgeInsetsGeometry get dimensions {
+    if (_widthIsUniform) {
+      return EdgeInsets.all(top.strokeInset);
+    }
+    return EdgeInsets.fromLTRB(
+      left.strokeInset,
+      top.strokeInset,
+      right.strokeInset,
+      bottom.strokeInset,
+    );
+  }
+
+  @override
+  bool get isUniform =>
+      _colorIsUniform &&
+      _widthIsUniform &&
+      _styleIsUniform &&
+      _strokeAlignIsUniform &&
+      _gradientIsUniform;
+
+  bool get _colorIsUniform {
+    final Color topColor = top.color;
+    return left.color == topColor &&
+        bottom.color == topColor &&
+        right.color == topColor;
+  }
+
+  bool get _widthIsUniform {
+    final double topWidth = top.width;
+    return left.width == topWidth &&
+        bottom.width == topWidth &&
+        right.width == topWidth;
+  }
+
+  bool get _styleIsUniform {
+    final BorderStyle topStyle = top.style;
+    return left.style == topStyle &&
+        bottom.style == topStyle &&
+        right.style == topStyle;
+  }
+
+  bool get _strokeAlignIsUniform {
+    final double topStrokeAlign = top.strokeAlign;
+    return left.strokeAlign == topStrokeAlign &&
+        bottom.strokeAlign == topStrokeAlign &&
+        right.strokeAlign == topStrokeAlign;
+  }
+
+  bool get _gradientIsUniform {
+    final topGradient = top.dashStyle;
+    return left.dashStyle == topGradient &&
+        bottom.dashStyle == topGradient &&
+        right.dashStyle == topGradient;
+  }
+
+  @override
+  DashedBorder? add(ShapeBorder other, {bool reversed = false}) {
+    if (other is DashedBorder &&
+        BorderSide.canMerge(top, other.top) &&
+        BorderSide.canMerge(right, other.right) &&
+        BorderSide.canMerge(bottom, other.bottom) &&
+        BorderSide.canMerge(left, other.left)) {
+      return DashedBorder.merge(this, other);
+    }
+    return null;
+  }
+
+  @override
+  DashedBorder scale(double t) => DashedBorder(
+        top: top.scale(t),
+        right: right.scale(t),
+        bottom: bottom.scale(t),
+        left: left.scale(t),
+      );
+
+  @override
+  ShapeBorder? lerpFrom(ShapeBorder? a, double t) {
+    if (a is DashedBorder) {
+      return DashedBorder.lerp(a, this, t);
+    }
+    return super.lerpFrom(a, t);
+  }
+
+  @override
+  ShapeBorder? lerpTo(ShapeBorder? b, double t) {
+    if (b is DashedBorder) {
+      return DashedBorder.lerp(this, b, t);
+    }
+    return super.lerpTo(b, t);
+  }
+
+  /// Linearly interpolate between two borders.
+  ///
+  /// If a border is null, it is treated as having four [BorderSide.none]
+  /// borders.
+  ///
+  /// {@macro dart.ui.shadow.lerp}
+  static DashedBorder? lerp(DashedBorder? a, DashedBorder? b, double t) {
+    if (identical(a, b)) {
+      return a;
+    }
+    if (a == null) {
+      return b!.scale(t);
+    }
+    if (b == null) {
+      return a.scale(1.0 - t);
+    }
+    return DashedBorder(
+      top: StyledBorderSide.lerp(a.top, b.top, t),
+      right: StyledBorderSide.lerp(a.right, b.right, t),
+      bottom: StyledBorderSide.lerp(a.bottom, b.bottom, t),
+      left: StyledBorderSide.lerp(a.left, b.left, t),
+    );
+  }
+
+  /// Paints the border within the given [Rect] on the given [Canvas].
+  ///
+  /// Uniform borders and non-uniform borders with similar colors and styles
+  /// are more efficient to paint than more complex borders.
+  ///
+  /// You can provide a [BoxShape] to draw the border on. If the `shape` in
+  /// [BoxShape.circle], there is the requirement that the border has uniform
+  /// color and style.
+  ///
+  /// If you specify a rectangular box shape ([BoxShape.rectangle]), then you
+  /// may specify a [BorderRadius]. If a `borderRadius` is specified, there is
+  /// the requirement that the border has uniform color and style.
+  ///
+  /// The [getInnerPath] and [getOuterPath] methods do not know about the
+  /// `shape` and `borderRadius` arguments.
+  ///
+  /// The `textDirection` argument is not used by this paint method.
+  ///
+  /// See also:
+  ///
+  ///  * [paintBorder], which is used if the border has non-uniform colors or styles and no borderRadius.
+  ///  * <https://pub.dev/packages/non_uniform_border>, a package that implements
+  ///    a Non-Uniform Border on ShapeBorder, which is used by Material Design
+  ///    buttons and other widgets, under the "shape" field.
+  @override
+  void paint(
+    Canvas canvas,
+    Rect rect, {
+    TextDirection? textDirection,
+    BoxShape shape = BoxShape.rectangle,
+    BorderRadius? borderRadius,
+  }) {
+    if (isUniform) {
+      switch (top.style) {
+        case BorderStyle.none:
+          return;
+        case BorderStyle.solid:
+          switch (shape) {
+            case BoxShape.circle:
+              assert(
+                borderRadius == null,
+                'A borderRadius cannot be given when shape is a BoxShape.circle.',
+              );
+              _paintUniformBorderWithCircle(canvas, rect, top);
+              break;
+            case BoxShape.rectangle:
+              if (borderRadius != null && borderRadius != BorderRadius.zero) {
+                _paintUniformBorderWithRadius(canvas, rect, top, borderRadius);
+                return;
+              }
+              _paintUniformBorderWithRectangle(canvas, rect, top);
+          }
+          return;
+      }
+    }
+
+    // Allow painting non-uniform borders if the color and style are uniform.
+    if (_colorIsUniform && _styleIsUniform && _gradientIsUniform) {
+      switch (top.style) {
+        case BorderStyle.none:
+          return;
+        case BorderStyle.solid:
+          _paintNonUniformBorder(
+            canvas,
+            rect,
+            shape: shape,
+            borderRadius: borderRadius,
+            textDirection: textDirection,
+            left: left,
+            top: top,
+            right: right,
+            bottom: bottom,
+          );
+          return;
+      }
+    }
+
+    assert(() {
+      if (borderRadius != null) {
+        throw FlutterError.fromParts(<DiagnosticsNode>[
+          ErrorSummary(
+            'A borderRadius can only be given on borders with uniform colors and styles.',
+          ),
+          ErrorDescription('The following is not uniform:'),
+          if (!_colorIsUniform) ErrorDescription('BorderSide.color'),
+          if (!_styleIsUniform) ErrorDescription('BorderSide.style'),
+        ]);
+      }
+      return true;
+    }());
+    assert(() {
+      if (shape != BoxShape.rectangle) {
+        throw FlutterError.fromParts(<DiagnosticsNode>[
+          ErrorSummary(
+            'A Border can only be drawn as a circle on borders with uniform colors and styles.',
+          ),
+          ErrorDescription('The following is not uniform:'),
+          if (!_colorIsUniform) ErrorDescription('BorderSide.color'),
+          if (!_styleIsUniform) ErrorDescription('BorderSide.style'),
+        ]);
+      }
+      return true;
+    }());
+    assert(() {
+      if (!_strokeAlignIsUniform ||
+          top.strokeAlign != BorderSide.strokeAlignInside) {
+        throw FlutterError.fromParts(<DiagnosticsNode>[
+          ErrorSummary(
+            'A Border can only draw strokeAlign different than BorderSide.strokeAlignInside on borders with uniform colors and styles.',
+          ),
+        ]);
+      }
+      return true;
+    }());
+
+    _paintBorder(
+      canvas,
+      rect,
+      top: top,
+      right: right,
+      bottom: bottom,
+      left: left,
+    );
+  }
+
+  @override
+  String toString() {
+    if (isUniform) {
+      return '${objectRuntimeType(this, 'Border')}.all($top)';
+    }
+    final List<String> arguments = <String>[
+      if (top != StyledBorderSide.none) 'top: $top',
+      if (right != StyledBorderSide.none) 'right: $right',
+      if (bottom != StyledBorderSide.none) 'bottom: $bottom',
+      if (left != StyledBorderSide.none) 'left: $left',
+    ];
+    return '${objectRuntimeType(this, 'GradientBorder')}(${arguments.join(", ")})';
+  }
+}
+
+void _paintBorder(
+  Canvas canvas,
+  Rect rect, {
+  StyledBorderSide top = StyledBorderSide.none,
+  StyledBorderSide right = StyledBorderSide.none,
+  StyledBorderSide bottom = StyledBorderSide.none,
+  StyledBorderSide left = StyledBorderSide.none,
+}) {
+  // We draw the borders as filled shapes, unless the borders are hairline
+  // borders, in which case we use PaintingStyle.stroke, with the stroke width
+  // specified here.
+  final Paint paint = Paint()..strokeWidth = 0.0;
+
+  final Path path = Path();
+
+  switch (top.style) {
+    case BorderStyle.solid:
+      paint.color = top.color;
+      path.reset();
+      path.moveTo(rect.left, rect.top);
+      path.lineTo(rect.right, rect.top);
+      if (top.width == 0.0) {
+        paint.style = PaintingStyle.stroke;
+      } else {
+        paint.style = PaintingStyle.fill;
+        path.lineTo(rect.right - right.width, rect.top + top.width);
+        path.lineTo(rect.left + left.width, rect.top + top.width);
+      }
+      canvas.drawPath(path, paint);
+      break;
+    case BorderStyle.none:
+      break;
+  }
+
+  switch (right.style) {
+    case BorderStyle.solid:
+      paint.color = right.color;
+      path.reset();
+      path.moveTo(rect.right, rect.top);
+      path.lineTo(rect.right, rect.bottom);
+      if (right.width == 0.0) {
+        paint.style = PaintingStyle.stroke;
+      } else {
+        paint.style = PaintingStyle.fill;
+        path.lineTo(rect.right - right.width, rect.bottom - bottom.width);
+        path.lineTo(rect.right - right.width, rect.top + top.width);
+      }
+      canvas.drawPath(path, paint);
+      break;
+    case BorderStyle.none:
+      break;
+  }
+
+  switch (bottom.style) {
+    case BorderStyle.solid:
+      paint.color = bottom.color;
+      path.reset();
+      path.moveTo(rect.right, rect.bottom);
+      path.lineTo(rect.left, rect.bottom);
+      if (bottom.width == 0.0) {
+        paint.style = PaintingStyle.stroke;
+      } else {
+        paint.style = PaintingStyle.fill;
+        path.lineTo(rect.left + left.width, rect.bottom - bottom.width);
+        path.lineTo(rect.right - right.width, rect.bottom - bottom.width);
+      }
+      canvas.drawPath(path, paint);
+      break;
+    case BorderStyle.none:
+      break;
+  }
+
+  switch (left.style) {
+    case BorderStyle.solid:
+      paint.color = left.color;
+      path.reset();
+      path.moveTo(rect.left, rect.bottom);
+      path.lineTo(rect.left, rect.top);
+      if (left.width == 0.0) {
+        paint.style = PaintingStyle.stroke;
+      } else {
+        paint.style = PaintingStyle.fill;
+        path.lineTo(rect.left + left.width, rect.top + top.width);
+        path.lineTo(rect.left + left.width, rect.bottom - bottom.width);
+      }
+      canvas.drawPath(path, paint);
+      break;
+    case BorderStyle.none:
+      break;
+  }
+}
+
+void _paintUniformBorderWithRadius(
+  Canvas canvas,
+  Rect rect,
+  StyledBorderSide side,
+  BorderRadius borderRadius,
+) {
+  assert(side.style != BorderStyle.none);
+  final Paint paint = Paint()..color = side.color;
+  final double width = side.width;
+  if (width == 0.0) {
+    paint
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 0.0;
+    canvas.drawRRect(borderRadius.toRRect(rect), paint);
+  } else {
+    final RRect borderRect = borderRadius.toRRect(rect);
+    final RRect inner = borderRect.deflate(side.strokeInset);
+    final RRect outer = borderRect.inflate(side.strokeOutset);
+    canvas.drawDRRect(outer, inner, paint);
+  }
+}
+
+void _paintNonUniformBorder(
+  Canvas canvas,
+  Rect rect, {
+  required BorderRadius? borderRadius,
+  required BoxShape shape,
+  required TextDirection? textDirection,
+  required StyledBorderSide left,
+  required StyledBorderSide top,
+  required StyledBorderSide right,
+  required StyledBorderSide bottom,
+}) {
+  final RRect borderRect;
+  switch (shape) {
+    case BoxShape.rectangle:
+      borderRect = (borderRadius ?? BorderRadius.zero)
+          .resolve(textDirection)
+          .toRRect(rect);
+      break;
+    case BoxShape.circle:
+      assert(
+        borderRadius == null,
+        'A borderRadius cannot be given when shape is a BoxShape.circle.',
+      );
+      borderRect = RRect.fromRectAndRadius(
+        Rect.fromCircle(center: rect.center, radius: rect.shortestSide / 2.0),
+        Radius.circular(rect.width),
+      );
+  }
+  final Paint paint = Paint()..color = top.color;
+  final RRect inner = _deflateRRect(
+    borderRect,
+    EdgeInsets.fromLTRB(
+      left.strokeInset,
+      top.strokeInset,
+      right.strokeInset,
+      bottom.strokeInset,
+    ),
+  );
+  final RRect outer = _inflateRRect(
+    borderRect,
+    EdgeInsets.fromLTRB(
+      left.strokeOutset,
+      top.strokeOutset,
+      right.strokeOutset,
+      bottom.strokeOutset,
+    ),
+  );
+  canvas.drawDRRect(outer, inner, paint);
+}
+
+RRect _inflateRRect(RRect rect, EdgeInsets insets) => RRect.fromLTRBAndCorners(
+      rect.left - insets.left,
+      rect.top - insets.top,
+      rect.right + insets.right,
+      rect.bottom + insets.bottom,
+      topLeft: (rect.tlRadius + Radius.elliptical(insets.left, insets.top))
+          .clamp(minimum: Radius.zero), // ignore_clamp_double_lint
+      topRight: (rect.trRadius + Radius.elliptical(insets.right, insets.top))
+          .clamp(minimum: Radius.zero), // ignore_clamp_double_lint
+      bottomRight:
+          (rect.brRadius + Radius.elliptical(insets.right, insets.bottom))
+              .clamp(minimum: Radius.zero), // ignore_clamp_double_lint
+      bottomLeft:
+          (rect.blRadius + Radius.elliptical(insets.left, insets.bottom))
+              .clamp(minimum: Radius.zero), // ignore_clamp_double_lint
+    );
+
+RRect _deflateRRect(RRect rect, EdgeInsets insets) => RRect.fromLTRBAndCorners(
+      rect.left + insets.left,
+      rect.top + insets.top,
+      rect.right - insets.right,
+      rect.bottom - insets.bottom,
+      topLeft: (rect.tlRadius - Radius.elliptical(insets.left, insets.top))
+          .clamp(minimum: Radius.zero), // ignore_clamp_double_lint
+      topRight: (rect.trRadius - Radius.elliptical(insets.right, insets.top))
+          .clamp(minimum: Radius.zero), // ignore_clamp_double_lint
+      bottomRight:
+          (rect.brRadius - Radius.elliptical(insets.right, insets.bottom))
+              .clamp(minimum: Radius.zero), // ignore_clamp_double_lint
+      bottomLeft:
+          (rect.blRadius - Radius.elliptical(insets.left, insets.bottom))
+              .clamp(minimum: Radius.zero), // ignore_clamp_double_lint
+    );
+
+void _paintUniformBorderWithCircle(
+  Canvas canvas,
+  Rect rect,
+  StyledBorderSide side,
+) {
+  assert(side.style != BorderStyle.none);
+  final double radius = (rect.shortestSide + side.strokeOffset) / 2;
+  canvas.drawCircle(
+    rect.center,
+    radius,
+    side.toPaint(),
+  );
+}
+
+void _paintUniformBorderWithRectangle(
+  Canvas canvas,
+  Rect rect,
+  StyledBorderSide side,
+) {
+  assert(side.style != BorderStyle.none);
+  canvas.drawRect(
+    rect.inflate(side.strokeOffset / 2),
+    side.toPaint(),
+  );
+}
---
+++ lib/src/ui/styled_border_side.dart
@@ -0,0 +1,314 @@
+import 'dart:math' as math;
+import 'dart:ui' as ui;
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/rendering.dart';
+
+class BorderDash {
+  const BorderDash(this.array, [this.offset = 0]);
+
+  static const none = BorderDash([0, 3]);
+  static const dotted = BorderDash([1]);
+  static const dashed = BorderDash([3, 2]);
+  static const morse = BorderDash([3, 2, 1, 2]);
+
+  final List<double> array;
+  final double offset;
+
+  static BorderDash? lerp(BorderDash? a, BorderDash? b, double t) {
+    if (a == null) return b;
+    if (b == null) return a;
+    final maxlen = math.max(a.array.length, b.array.length);
+    final arraya = maxlen > a.array.length
+        ? List.generate(maxlen, (i) => i >= a.array.length ? 0 : a.array[i])
+        : a.array;
+    final arrayb = maxlen > b.array.length
+        ? List.generate(maxlen, (i) => i >= b.array.length ? 0 : b.array[i])
+        : b.array;
+
+    return BorderDash(
+      [
+        for (int i = 0; i < maxlen; i++)
+          if (i % 2 == 0)
+            ui.lerpDouble(arraya[i], arrayb[i], t) ?? 0
+          else
+            ui.lerpDouble(arrayb[i], arraya[i], t) ?? 0,
+      ],
+      ui.lerpDouble(a.offset, b.offset, t) ?? 0,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is BorderDash &&
+        listEqual(other.array, array) &&
+        other.offset == offset;
+  }
+
+  static bool listEqual(List a, List b) {
+    if (a.runtimeType != b.runtimeType) {
+      return false;
+    }
+    if (a.length != b.length) {
+      return false;
+    }
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @override
+  int get hashCode => Object.hash(array, offset);
+}
+
+@immutable
+class StyledBorderSide extends BorderSide {
+  /// Creates the side of a border.
+  ///
+  /// By default, the border is 1.0 logical pixels wide and solid black.
+  const StyledBorderSide({
+    super.color = const Color(0xFF000000),
+    super.width = 1.0,
+    super.style = BorderStyle.solid,
+    super.strokeAlign = BorderSide.strokeAlignInside,
+    this.dashStyle,
+  }) : assert(width >= 0.0);
+
+  /// Creates a [StyledBorderSide] that represents the addition of the two given
+  /// [StyledBorderSide]s.
+  ///
+  /// It is only valid to call this if [canMerge] returns true for the two
+  /// sides.
+  ///
+  /// If one of the sides is zero-width with [BorderStyle.none], then the other
+  /// side is return as-is. If both of the sides are zero-width with
+  /// [StyledBorderSide.none], then [StyledBorderSide.none] is returned.
+  ///
+  /// The arguments must not be null.
+  static StyledBorderSide merge(BorderSide a, BorderSide b) {
+    assert(BorderSide.canMerge(a, b));
+    final bool aIsNone = a.style == BorderStyle.none && a.width == 0.0;
+    final bool bIsNone = b.style == BorderStyle.none && b.width == 0.0;
+    if (aIsNone && bIsNone) {
+      return StyledBorderSide.none;
+    }
+    if (aIsNone) {
+      return a is StyledBorderSide
+          ? a
+          : StyledBorderSide(
+              color: b.color,
+              width: b.width,
+              style: b.style,
+              strokeAlign: b.strokeAlign,
+            );
+    }
+    if (bIsNone) {
+      return b is StyledBorderSide
+          ? b
+          : StyledBorderSide(
+              color: a.color,
+              width: a.width,
+              style: a.style,
+              strokeAlign: a.strokeAlign,
+            );
+    }
+    assert(a.color == b.color);
+    assert(a.style == b.style);
+    return StyledBorderSide(
+      color: a.color, // == b.color
+      width: a.width + b.width,
+      strokeAlign: math.max(a.strokeAlign, b.strokeAlign),
+      style: a.style, // == b.style
+      dashStyle: a is StyledBorderSide ? a.dashStyle : null,
+    );
+  }
+
+  final BorderDash? dashStyle;
+
+  /// A hairline black border that is not rendered.
+  static const StyledBorderSide none =
+      StyledBorderSide(width: 0.0, style: BorderStyle.none);
+
+  /// Creates a copy of this border but with the given fields replaced with the new values.
+  @override
+  StyledBorderSide copyWith({
+    Color? color,
+    double? width,
+    BorderStyle? style,
+    double? strokeAlign,
+    BorderDash? dashStyle,
+  }) =>
+      StyledBorderSide(
+        color: color ?? this.color,
+        width: width ?? this.width,
+        style: style ?? this.style,
+        strokeAlign: strokeAlign ?? this.strokeAlign,
+        dashStyle: dashStyle ?? this.dashStyle,
+      );
+
+  @override
+  StyledBorderSide scale(double t) => StyledBorderSide(
+        color: color,
+        width: math.max(0.0, width * t),
+        style: t <= 0.0 ? BorderStyle.none : style,
+        dashStyle: dashStyle,
+      );
+
+  /// Linearly interpolate between two border sides.
+  ///
+  /// The arguments must not be null.
+  ///
+  /// {@macro dart.ui.shadow.lerp}
+  static StyledBorderSide lerp(BorderSide a, BorderSide b, double t) {
+    if (identical(a, b)) {
+      return a is StyledBorderSide
+          ? a
+          : StyledBorderSide(
+              color: a.color,
+              width: a.width,
+              style: a.style,
+              strokeAlign: a.strokeAlign,
+            );
+    }
+    if (t == 0.0) {
+      return a is StyledBorderSide
+          ? a
+          : StyledBorderSide(
+              color: a.color,
+              width: a.width,
+              style: a.style,
+              strokeAlign: a.strokeAlign,
+            );
+    }
+    if (t == 1.0) {
+      return b is StyledBorderSide
+          ? b
+          : StyledBorderSide(
+              color: b.color,
+              width: b.width,
+              style: b.style,
+              strokeAlign: b.strokeAlign,
+            );
+    }
+    final double width = ui.lerpDouble(a.width, b.width, t)!;
+    if (width < 0.0) {
+      return StyledBorderSide.none;
+    }
+    if (a.style == b.style && a.strokeAlign == b.strokeAlign) {
+      return StyledBorderSide(
+        color: Color.lerp(a.color, b.color, t)!,
+        width: width,
+        style: a.style, // == b.style
+        strokeAlign: a.strokeAlign, // == b.strokeAlign
+        dashStyle: BorderDash.lerp(
+          a is StyledBorderSide ? a.dashStyle : null,
+          b is StyledBorderSide ? b.dashStyle : null,
+          t,
+        ),
+      );
+    }
+    final Color colorA, colorB;
+    switch (a.style) {
+      case BorderStyle.solid:
+        colorA = a.color;
+        break;
+      case BorderStyle.none:
+        colorA = a.color.withAlpha(0x00);
+    }
+    switch (b.style) {
+      case BorderStyle.solid:
+        colorB = b.color;
+        break;
+      case BorderStyle.none:
+        colorB = b.color.withAlpha(0x00);
+    }
+    if (a.strokeAlign != b.strokeAlign) {
+      return StyledBorderSide(
+        color: Color.lerp(colorA, colorB, t)!,
+        width: width,
+        strokeAlign: ui.lerpDouble(a.strokeAlign, b.strokeAlign, t)!,
+        dashStyle: BorderDash.lerp(
+          a is StyledBorderSide ? a.dashStyle : null,
+          b is StyledBorderSide ? b.dashStyle : null,
+          t,
+        ),
+      );
+    }
+    return StyledBorderSide(
+      color: Color.lerp(colorA, colorB, t)!,
+      width: width,
+      strokeAlign: a.strokeAlign, // == b.strokeAlign
+      dashStyle: BorderDash.lerp(
+        a is StyledBorderSide ? a.dashStyle : null,
+        b is StyledBorderSide ? b.dashStyle : null,
+        t,
+      ),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is StyledBorderSide &&
+        other.color == color &&
+        other.width == width &&
+        other.style == style &&
+        other.strokeAlign == strokeAlign &&
+        other.dashStyle == dashStyle;
+  }
+
+  @override
+  int get hashCode => Object.hash(color, width, style, strokeAlign, dashStyle);
+
+  @override
+  String toStringShort() => 'StyledBorderSide';
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(
+        DiagnosticsProperty<Color>(
+          'color',
+          color,
+          defaultValue: const Color(0xFF000000),
+        ),
+      )
+      ..add(DoubleProperty('width', width, defaultValue: 1.0))
+      ..add(
+        DoubleProperty(
+          'strokeAlign',
+          strokeAlign,
+          defaultValue: BorderSide.strokeAlignInside,
+        ),
+      )
+      ..add(
+        EnumProperty<BorderStyle>(
+          'style',
+          style,
+          defaultValue: BorderStyle.solid,
+        ),
+      )
+      ..add(
+        DiagnosticsProperty<BorderDash>(
+          'dashStyle',
+          dashStyle,
+          defaultValue: null,
+        ),
+      );
+  }
+}
